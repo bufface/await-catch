c8176a37839c221e6dd2677f8170923244d4217b
Cristian Buffa <cristian.buffa@valtech.com>
2018-02-15 22:13:05 -0300

    Added files

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const err = (promise) => {
+  const handleError = err => [ err ];
+  const handleSuccess = data => [null, data];
+
+  if (Array.isArray(promise)) {
+    return Promise.all(promise)
+      .then(handleSuccess)
+      .catch(handleError);
+  }
+
+  return promise
+    .then(handleSuccess)
+    .catch(handleError);
+};
+
+export default err;
