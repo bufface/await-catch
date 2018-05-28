22f3e1aae8f285d1580ff51a6eaca56aeeac2b04
Cristian Buffa <cristian.buffa@valtech.com>
2018-05-28 11:49:36 -0300

    Fixed minior bug

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-const error = async (promise) => {
+import "babel-polyfill";
+
+const error = async promise => {
   try {
-    const data = Array.isArray(promise) ? await Promise.all(promise) : await promise;
-    return [ null, data ];
+    const data = Array.isArray(promise)
+      ? await Promise.all(promise)
+      : await promise;
+    return [null, data];
   } catch (err) {
-    return [ err ];
+    return [err];
   }
 };
 
